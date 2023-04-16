7662032009715d3922772db4692c7ecc8c9c243f
Abanoub Nassem <AbanoubNassemMousa@gmail.com>
2023-04-16 19:52:32 +0400

    =fix nuget not copying the Locales to the output directory

--- Trinity-React/src/utilities/trinity_localizer.ts
+++ Trinity-React/src/utilities/trinity_localizer.ts
@@ -3,12 +3,12 @@ class TrinityLocalizer {
     private readonly fallbackLocale: { [key: string]: string } = {};
 
     constructor(locale: { [key: string]: string }, fallbackLocale: { [key: string]: string }) {
-        this.locale = locale;
-        this.fallbackLocale = fallbackLocale;
+        this.locale = locale ?? {};
+        this.fallbackLocale = fallbackLocale ?? {};
     }
 
     localize(key: string, ...args: Array<string>): string {
-        let value = this.locale[key] || this.fallbackLocale[key] || key;
+        let value = this.locale[key] ?? this.fallbackLocale[key] ?? key;
         for (let i = 0; i < args.length; i++) {
             value = value.replace(`{${i}}`, args[i]);
         }
